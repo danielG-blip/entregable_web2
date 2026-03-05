0981d74ce9520ccc2ab4455aea4eadc337cc1308
danielG-blip <jgiraldo1@cesde.net>
2026-03-05 15:37:15 -0500

    Creacion de Categoria.java

--- src/main/java/com/tienda/lukingStore/model/Categoria.java
+++ src/main/java/com/tienda/lukingStore/model/Categoria.java
@@ -1,1 +1,60 @@
+package com.tienda.lukingStore.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import scala.collection.immutable.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "categorias")
+
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name", nullable = false, length = 20)
+    private String nombre;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Producto> productos;
+
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+    if (id != null && id > 0) {
+        this.id = id.intValue();
+    } else {
+        System.out.println("El id no puede estar vacío");
+    }
+}
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Producto> getProductos() {
+        return this.productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
+
+}
